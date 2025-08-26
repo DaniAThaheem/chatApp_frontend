bbcaf239aa2f0cdf335d02d2e33c2a22861346d7
DaniAThaheem <danimsbzuacc@outlook.com>
2025-08-26 18:03:08 +0500

    Created Login And Register Page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAuth } from '../context/AuthContext'
+import { LockClosedIcon } from '@heroicons/react/20/solid'
+import { Navigate } from 'react-router-dom'
+
 
 const Login = () => {
+
+  const {login} = useAuth()
+  const [data, setData ]= useState(
+    {
+      email:"",
+      password:""
+    }
+  )
+
+  const handleDataChange = (name)=>(e)=>{
+    setData(
+      {
+        ...data,
+        [name]:e.target.value
+      }
+    )
+  }
+
+  const handleLogin = async () =>{
+    await login(data)
+
+  } 
   return (
-    <div>
-      
+    <div className=''>
+      <h1 className=''>Chat App</h1>
+      <div className=''>
+        <h1 className="">
+          <LockClosedIcon className=''/>Login
+        </h1>
+        <Input
+        placeholder ="Enter email..."
+        type="email"
+        value={data.email}
+        onChange={handleDataChange("email")}
+         />
+        <Input 
+        placeholder ="Enter password"
+        type="passsword"
+        value={data.password}
+        onChange={handleDataChange("passsword")}
+        />
+        <Button
+        disabled={Object.values(data).some((value)=>!value)}
+        fullWidth
+        onClick={handleLogin}
+        >
+          Register
+        </Button>
+        <small className=''>
+          Don&apos;t have an account?
+          <Navigate className="" to={"/register"}>
+            Register
+          </Navigate>
+        </small>
+
+      </div>
     </div>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import { LockClosedIcon } from '@heroicons/react/20/solid'
+import React, { useState } from 'react'
+import { Navigate } from 'react-router-dom'
+import { useAuth } from '../context/AuthContext'
 
 const Register = () => {
+  const [data, setData] = useState(
+    {
+      email:"",
+      username:"",
+      password:""
+    }
+  )
+  const {register}  = useAuth()
+
+  const handleDataChange = (name)=>(e)=>{
+    setData({
+      ...data,
+      [name]:e.target.value
+  })
+  }
+
+  const handleRegister = async() => {await register(data)}
   return (
-    <div>
-      
+    <div className=''>
+      <h1 className=''>Chat App</h1>
+      <div className=''>
+        <h1 className="">
+          <LockClosedIcon className='' />Register
+        </h1>
+        <Input
+        placeholder ="Enter email..."
+        type="email"
+        value={data.email}
+        onChange={handleDataChange("email")}
+         />
+        <Input
+        placeholder ="Enter username"
+        value={data.username}
+        onChange={handleDataChange("username")}
+        />
+        <Input 
+        placeholder ="Enter password"
+        type="password"
+        value={data.password}
+        onChange={handleDataChange("password")}
+        />
+        <Button
+        disabled={Object.values(data).some((value)=>!value)}
+        fullWidth
+        onClick={handleRegister}
+        >
+          Register
+        </Button>
+        <small className=''>
+          Already have an account?
+          <Navigate className="" to={"/login"}>
+            Login
+          </Navigate>
+        </small>
+
+      </div>
     </div>
   )
 }

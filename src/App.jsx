2eb288055620b771765f6100477f1b60ae849f3b
DaniAThaheem <danimsbzuacc@outlook.com>
2025-08-26 18:19:30 +0500

    Setup Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,64 @@
 import './App.css'
+import Chat from './pages/Chat'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import { useAuth } from './context/AuthContext'
+import { Routes, Route, Navigate,  } from 'react-router-dom'
+import PublicRoute from "./components/basic/PublicRoute"
+import PrivateRoute from "./components/basic/PrivateRoute"
 
 function App() {
+  const {user, token} = useAuth()
 
   return (
     <>
-      
+      <Routes>
+        <Route
+        path='/'
+        element={
+          token && user?
+          <Navigate to={"/chat"} />
+          :
+          <Navigate to ={"/login"} />
+        }
+        >
+        </Route>
+
+        <Route
+        path='/chat'
+        element={
+          <PrivateRoute>
+            <Chat />
+          </PrivateRoute>
+        }
+        />
+        <Route
+        path='/login'
+        element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        }
+        />
+        <Route
+        path='/register'
+        element={
+          <PublicRoute>
+            <Register />
+          </PublicRoute>
+        }
+        />
+        <Route
+        path='*'
+        element={
+          <p>
+            Page Not Found
+          </p>
+        }
+        />
+
+
+      </Routes>
     </>
   )
 }
